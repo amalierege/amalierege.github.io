352c2096fc50479d30a1445d5f5eb9f467ea4f2c
amalierege <a.rege@hotmail.com>
2026-01-23 00:05:34 +0100

    welcome message styled with tab icon and name

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,26 @@ function App() {
           come back soon! :3
         </p>
       </div>
+      <footer>
+        {footer()}
+      </footer>
     </>
+  )
+}
+
+function footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>
+        <img className="dangoImage" src="./src/assets/dango.png" alt="Image of a the japanese sweet Dango"/>
+        <br/>
+        <a className="iconAttribute" href="https://www.flaticon.com/free-icons/japan" title="japan icons">Japan Icons created by Freepik - Flaticon</a>
+      </p>
+      <p>
+        &copy; {year} Tamapo Studio
+      </p>
+    </footer>
   )
 }
 
